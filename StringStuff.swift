857157b1045cd422b6bb247673b22f355874fff2
Ina Tolo <94471679+InaTolo@users.noreply.github.com>
2022-04-10 23:10:37 +0000

    Finished code for the StringStuff.swift program.

--- StringStuff.swift
+++ StringStuff.swift
@@ -2,21 +2,24 @@
 // StringStuff.swift
 //
 // Created by Ina Tolo
-// Created on 2022-3-28
+// Created on 2022-4-1
 // Version 1.0
 // Copyright (c) 2022 Ina Tolo. All rights reserved.
 //
 // The StringStuff program implements an application that
-//  uses a function to alter the contents of a string based
-// on the number characters present.
+// uses a function to alter the contents of a string and
+// returns a version of the original string passed as
+// as an argument.
+
 import Foundation
 
-// uses recursion to reverse a string
+// alters the arrangement of a string
 func blowup(stringFromFile: String) -> String {
     // declaring variables
     var newString: String = ""
     var charInt: Int
     let tempArray: [Character] = Array(stringFromFile)
+    var placeHolder: Int = 0
 
     // determines which character(s) in the array needs to be altered
     for cursor in 0..<tempArray.count - 1 {
@@ -28,10 +31,13 @@ func blowup(stringFromFile: String) -> String {
                 try catchString()
             }
 
+            // repeats character that is after an integer
             for numCount in 0..<charInt {
                 newString += (String) (tempArray[cursor + 1])
+                placeHolder += numCount
             }
 
+            // checks last character in array
             if cursor == tempArray.count - 2 {
                 do {
                    charInt = (Int) ((String) (tempArray[cursor + 1])) ?? 0
@@ -47,6 +53,7 @@ func blowup(stringFromFile: String) -> String {
         } catch {
             newString += (String) (tempArray[cursor])
 
+            // checks last character in array
             if cursor == tempArray.count - 2 {
                 do {
                     charInt = (Int) ((String) (tempArray[cursor + 1])) ?? 0
@@ -63,6 +70,7 @@ func blowup(stringFromFile: String) -> String {
         }
     }
 
+    // checks if a single character is altered
     if tempArray.count == 1 {
         do {
             charInt = (Int) ((String) (tempArray[0])) ?? 0
@@ -104,11 +112,12 @@ print()
 let listOftrings: String = try String(contentsOfFile: stringsLocation)
 let stringsArrayFile: [String] = listOftrings.components(separatedBy: "\n")
 
+// calls function and passes each element in the array
 for loopCounter in 0..<stringsArrayFile.count {
     if stringsArrayFile[loopCounter].count == 0 {
         alteredList.append("\n")
         continue
-    } 
+    }
     blowupStringUser = blowup(stringFromFile: stringsArrayFile[loopCounter])
     if loopCounter != stringsArrayFile.count - 1 {
         alteredList.append(blowupStringUser + "\n")
@@ -118,11 +127,14 @@ for loopCounter in 0..<stringsArrayFile.count {
     blowupStringUser = blowup(stringFromFile: stringsArrayFile[loopCounter])
 }
 
-
-// converts list of reversed strings to an array
+// converts list of altered strings to an array
 let alteredStringsArray: [String] = alteredList.components(separatedBy: "\n")
 
-// writes the new reversed strings to the output file
+// displays message that alerts user file is done being altered
+print("Done altering strings. Look below and check the output file.")
+print("--------------------------")
+
+// writes the new altered strings to the output file
 try text.write(to: URL(fileURLWithPath: "/home/runner/Assign-02-Swift/output.txt"),
     atomically: false, encoding: .utf8)
 
@@ -131,8 +143,10 @@ if let fileWriter = try? FileHandle(forUpdating:
     // adds each string to the output file
     for stringFormat in 0..<alteredStringsArray.count {
         if stringFormat != alteredStringsArray.count - 1 {
+            print(alteredStringsArray[stringFormat])
             arrayToString = alteredStringsArray[stringFormat] + "\n"
         } else {
+            print(alteredStringsArray[stringFormat])
             arrayToString = alteredStringsArray[stringFormat]
         }
         fileWriter.seekToEndOfFile()
@@ -140,4 +154,4 @@ if let fileWriter = try? FileHandle(forUpdating:
     }
     fileWriter.closeFile()
 }
-print("Done reversing. Check the output file.")
+print("--------------------------")
